b4d124704adc7041222f917f57dd4024d6740972
Jin Ah Kim <108795647+whatareyoudoingz@users.noreply.github.com>
2023-01-16 21:56:36 +0900

    [Silver IV] Title: 한수, Time: 44 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Silver/1065. 한수/한수.py
@@ -0,0 +1,27 @@
+def han(a):
+    global result
+
+    a=list(str(i))
+    if len(a)==1 or len(a)==2:
+        result+=1
+    for j in range(len(a)):
+        if j==len(a)-1:
+            a.remove(a[-1])
+        else:
+            a[j]=int(a[j+1])-int(a[j])
+
+    for k in range(len(a)):
+        if k == len(a)-1:
+            break
+        elif a[k +1]!=a[k]:
+            break
+        else:
+            result+=1
+    return result
+
+
+x=int(input())
+result=0
+for i in range(1,x+1):
+    han(i)
+print(result)
