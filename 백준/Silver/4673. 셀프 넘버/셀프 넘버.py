46de72aaa74114e3a9db2fc607a623113ebf1718
Jin Ah Kim <108795647+whatareyoudoingz@users.noreply.github.com>
2023-01-16 19:43:20 +0900

    Update 셀프 넘버.py

--- 백준/Silver/4673. 셀프 넘버/셀프 넘버.py
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.py
@@ -1,14 +1,16 @@
 def d(n):
-    num = list(str(n))
-    asw = n
-    for i in range(len(num)):
-        asw += int(num[i])
-    return asw
+    answer=n
+    for i in range(len(str(n))):
+        answer+=int(str(n)[i])
+    return answer
+        
 
-SET = list(range(1,10001))
+lis=list(range(1,10001))
 for n in range(1,10001):
-    if d(n) in SET:
-        SET.remove(d(n))
+    if d(n) in lis:
+        lis.remove(d(n))
+
+for k in lis:
+    print(k)
     
-for i in range(len(SET)):
-    print(SET[i])
+# 알고리즘을 짜는 것보다 함수로 변환하는 게 더 어렵다.... 함수로 구현하는 연습해야겠다
