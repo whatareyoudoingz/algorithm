b0ccf06e6e99288d84793424e6787321a83f37b0
Jin Ah Kim <108795647+whatareyoudoingz@users.noreply.github.com>
2023-01-17 21:45:04 +0900

    [Bronze I] Title: 단어 공부, Time: 96 ms, Memory: 32416 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -0,0 +1,15 @@
+s=input().upper()
+total=0 # 최대 카운트 수
+tt=[] # 최대 문자
+for i in set(s): #하나씩 살펴보자
+    if total < s.count(i) : #문자 카운트 수가 최대보다 크면
+        total = s.count(i) #최대 카운트 수를 바꾸자
+        tt.clear()
+        tt.append(i) # 최대 문자
+    elif total == s.count(i):
+        tt.append(i) # 최대 문자
+
+if len(tt)==1:
+    print(tt[0])
+else:
+    print("?")
