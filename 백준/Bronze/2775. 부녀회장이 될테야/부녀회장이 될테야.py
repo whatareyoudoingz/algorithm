f0a734ce7c21fa2839d157d50ea6eccdca90b958
Jin Ah Kim <108795647+whatareyoudoingz@users.noreply.github.com>
2023-01-18 23:05:53 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 56 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
@@ -0,0 +1,9 @@
+T=int(input())
+for _ in range(T):
+    k=int(input()) #층
+    n=int(input()) #호
+    storage=[[i for i in range(1,n+1)] for _ in range(k+1)]
+    for x in range(1,k+1):
+        for y in range(1,n):
+            storage[x][y]=storage[x][y-1]+storage[x-1][y]
+    print(storage[k][n-1])
