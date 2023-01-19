529295899c719546b8df816109204f5a1435484b
Jin Ah Kim <108795647+whatareyoudoingz@users.noreply.github.com>
2023-01-19 14:42:20 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 4052 ms, Memory: 47664 KB -BaekjoonHub

---
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.py
@@ -0,0 +1,10 @@
+n=int(input())
+dic={}
+for _ in range(n):
+    name, active=input().split()
+    if name not in dic:
+        dic[name]=active
+    else:
+        dic.pop(name)
+for word in sorted(dic.keys(),reverse=True):
+    print(word)
