9abc988fb74afcfc8a3376a500bc6d7151ea3a81
Jin Ah Kim <108795647+whatareyoudoingz@users.noreply.github.com>
2023-01-26 11:57:16 +0900

    [Silver V] Title: 카드1, Time: 40 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/2161. 카드1/카드1.py
+++ 백준/Silver/2161. 카드1/카드1.py
@@ -1,9 +1,6 @@
-total=[i for i in range(1,int(input())+1)]
+total=list(range(1,int(input())+1))
 lis=[]
 while len(total)!=1:
-    lis.append(total[0])
-    total.pop(0)
-    t=total[0]
-    total.pop(0)
-    total.append(t)
-print(" ".join(str(i) for i in lis+total))
+    lis.append(total.pop(0))
+    total.append(total.pop(0))
+print(*lis, total[0])
