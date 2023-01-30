f30a52878a15d6313e97b4c31ebb6d772fd7839a
Jin Ah Kim <108795647+whatareyoudoingz@users.noreply.github.com>
2023-01-30 17:59:38 +0900

    [Bronze III] Title: 네 번째 점, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.py
@@ -0,0 +1,14 @@
+lis_x, lis_y=[],[]
+
+for _ in range(3):
+    a,b=input().split()
+    lis_x.append(a)
+    lis_y.append(b)
+
+for k in lis_x:
+    if lis_x.count(k)<2:
+        x=k
+for q in lis_y:
+    if lis_y.count(q)<2:
+        y=q
+print(x,y)
