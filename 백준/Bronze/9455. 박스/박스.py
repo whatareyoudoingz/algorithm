790c40bf681f9eb6a67cf941156bb2f7a99130b9
Jin Ah Kim <108795647+whatareyoudoingz@users.noreply.github.com>
2023-01-31 16:19:51 +0900

    [Bronze I] Title: 박스, Time: 588 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/9455. 박스/박스.py
+++ 백준/Bronze/9455. 박스/박스.py
@@ -1,22 +1,20 @@
+def box_checker(a):
+    global i,j,result
+    for i in range(a):
+        if box[i][j]=='1': 
+            for k in range(i+1,a):
+                if box[k][j]=='0': 
+                    result+=1
+        if box[i][j]=='0': 
+            continue
+
 for _ in range(int(input())):
     result=0
     m,n=map(int,input().split())
     box=[list(input().split()) for _ in range(m)]
     for j in range(n):
         if box[m-1][j]=='1':
-            for i in range(m-1):
-                if box[i][j]=='1': 
-                    for k in range(i+1,m-1):
-                        if box[k][j]=='0': 
-                            result+=1
-                if box[i][j]=='0': 
-                    continue
+            box_checker(m-1)
         else: 
-            for i in range(m):
-                if box[i][j]=='1': 
-                    for k in range(i+1,m):
-                        if box[k][j]=='0': 
-                            result+=1
-                if box[i][j]=='0': 
-                    continue
+            box_checker(m)
     print(result)
