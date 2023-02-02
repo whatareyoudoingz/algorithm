41bd6f656e2b9e8adb9a2b33534671cb34eb0194
Jin Ah Kim <108795647+whatareyoudoingz@users.noreply.github.com>
2023-02-02 16:53:32 +0900

    [Bronze I] Title: 오르막길, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2846. 오르막길/오르막길.py
+++ 백준/Bronze/2846. 오르막길/오르막길.py
@@ -1,17 +1,17 @@
 T=int(input())
 road=list(map(int,input().split()))
+
 short, long=road[0], 0
 i, ans=1, []
 while True:
     if i==T:
-        if road[i-1]>road[i-2]:
-            long=road[i-1] 
-            h=long-short 
-            ans.append(h)
+        long=road[i-1] 
+        h=long-short 
+        ans.append(h)
         break
-    if road[i]==road[i-1]:
+    if road[i]==road[i-1]: #평지 : 리셋
         short=road[i]
-    if road[i]<road[i-1]:
+    if road[i]<road[i-1]: #내리막길
         long=road[i-1] 
         h=long-short 
         ans.append(h)
