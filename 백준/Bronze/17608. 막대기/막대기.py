2ff6b8e5ff50d63404c53da22283e457a2d74c8e
Jin Ah Kim <108795647+whatareyoudoingz@users.noreply.github.com>
2023-02-07 17:00:39 +0900

    [Bronze II] Title: 막대기, Time: 84 ms, Memory: 35108 KB -BaekjoonHub

---
+++ 백준/Bronze/17608. 막대기/막대기.py
@@ -0,0 +1,12 @@
+import sys
+
+n=int(sys.stdin.readline())
+tall=list(int(sys.stdin.readline()) for _ in range(n))
+result=1
+maxx=tall[-1]
+for i in range(n-1,-1,-1):
+    if tall[i]>maxx:
+        maxx=tall[i]
+        result+=1
+
+print(result)
