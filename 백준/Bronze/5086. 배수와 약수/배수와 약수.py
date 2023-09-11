af02ad830cce0d89d0fc0f4d9492db0071085b76
Jin Ah Kim <108795647+whatareyoudoingz@users.noreply.github.com>
2023-09-11 23:58:23 +0900

    [Bronze III] Title: 배수와 약수, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.py
@@ -0,0 +1,13 @@
+while True:
+    try:
+        a,b=map(int,input().split())
+        if a==b==0:
+            break
+        elif b%a==0:
+            print("factor")
+        elif a%b==0:
+            print("multiple")
+        else:
+            print("neither")
+    except:
+        break
