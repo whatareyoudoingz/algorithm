18d7fadc6231011092a96110b3065b5b61a9211f
Jin Ah Kim <108795647+whatareyoudoingz@users.noreply.github.com>
2023-09-12 16:48:27 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 56 ms, Memory: 31388 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
@@ -1,10 +1,6 @@
 n,m=map(int,input().split())
-ans=[]
-for _ in range(n*2):
-    ans.append(list(map(int,input().split())))
+ans=[list(map(int,input().split())) for _ in range(n*2)]
 for i in range(n):
     for j in range(m):
-        if j==m-1:
-            print(ans[i][j]+ans[i+n][j])
-        else:
-            print(ans[i][j]+ans[i+n][j],end=" ")
+        print(ans[i][j]+ans[i+n][j],end=" ")
+    print()
